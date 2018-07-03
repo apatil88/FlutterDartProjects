d21b1328b4e235ac593cb0646063738d87295762
Amrut Patil <apatil@emmes.com>
2018-07-03 13:54:35 -0400

    Adding scaffold layout

--- pics/lib/main.dart
+++ pics/lib/main.dart
@@ -1,7 +1,23 @@
 //Import a helper library from Flutter to get content on the screen
+import 'package:flutter/material.dart';
 
 //Define a 'main' function to run when the app starts
+void main() {
+  //Create a new Text widget to show some text on the screen
+  var app = MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Lets see some images!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('Hi there!');
+        },
+        child: Icon(Icons.add),
+      ),
+    ),
+  );
 
-//Create a new Text widget to show some text on the screen
-
-//Take that widget and get it on the screen.
+  //Take that widget and get it on the screen.
+  runApp(app);
+}
