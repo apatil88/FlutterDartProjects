d77f95790db103c862ba017a6ec872544cb3a0f6
Amrut Patil <apatil@emmes.com>
2018-07-05 16:01:07 -0400

    Fetching image data

--- pics/lib/src/app.dart
+++ pics/lib/src/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get; //We only need this function
 
 class App extends StatefulWidget {
   @override
@@ -10,6 +11,11 @@ class App extends StatefulWidget {
 class AppState extends State<App> {
   int counter = 0;
 
+  void fetchImage() {
+    counter += 1;
+    get('http://jsonplaceholder.typicode.com/photos/${counter}');
+  }
+
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
@@ -17,11 +23,7 @@ class AppState extends State<App> {
           title: Text('Lets see some images!'),
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              counter += 1;
-            });
-          },
+          onPressed: fetchImage, //Call the function to fetch image data
           child: Icon(Icons.add),
         ),
         body: Text('${counter} images'),
