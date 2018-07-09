e4676adc060c0cf6a9105a4a74284931dc39d2b5
Amrut Patil <apatil@emmes.com>
2018-07-09 11:44:03 -0400

    Flutter App to demonstrate form validation

---
+++ login_stateful/lib/src/screens/login_screen.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Form(
+        //Layout Widget
+        child: Column(
+          children: <Widget>[
+            emailField(),
+            passwordField(),
+            Container(
+              margin: EdgeInsets.only(top: 25.0),
+            ),
+            submitButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'you@example.com',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    return TextFormField(
+      obscureText: false,
+      decoration: InputDecoration(
+        labelText: 'Password',
+        hintText: 'Password',
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return RaisedButton(
+      child: Text('Submit'),
+      color: Colors.blue,
+      onPressed: () {},
+    );
+  }
+}
