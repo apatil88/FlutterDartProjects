07853e3d805b4e4c6a17c43c2c95e0a0a2ce5430
Amrut Patil <apatil@emmes.com>
2018-07-17 15:50:08 -0400

    Adding flaps to the box and animating the flaps

--- flutter_animation/lib/src/screens/home.dart
+++ flutter_animation/lib/src/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/cat.dart';
+import 'dart:math';
 
 class Home extends StatefulWidget {
   @override
@@ -13,10 +14,38 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
   AnimationController
       catController; //To perform and set the duration of the animation
 
+  Animation<double> boxAnimation;
+  AnimationController boxController;
+
   //Lifecycle method called when a new instance of HomeState is created. Only available for classes which extend the State class.
   initState() {
     //initState() is actually defined in the superclass State.
     super.initState();
+
+    boxController = AnimationController(
+      duration: Duration(milliseconds: 300),
+      vsync: this,
+    );
+
+    boxAnimation = Tween(
+      begin: pi * 0.6,
+      end: pi * 0.65,
+    ).animate(
+      CurvedAnimation(
+        parent: boxController,
+        curve: Curves.linear,
+      ),
+    );
+
+    boxAnimation.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        boxController.reverse();
+      } else if (status == AnimationStatus.dismissed) {
+        boxController.forward();
+      }
+    });
+
+    boxController.forward();
 
     catController = AnimationController(
       duration: Duration(milliseconds: 200),
@@ -35,29 +64,36 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
   onTap() {
     //Start the animation
     if (catController.status == AnimationStatus.completed) {
+      //If the cat is going into the box, move the flaps.
+      boxController.forward();
       catController.reverse();
     } else if (catController.status == AnimationStatus.dismissed) {
+      //If the cat is coming out of the box, freeze the flaps.
+      boxController.stop();
       catController.forward();
     }
   }
 
   Widget build(context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Cat\'s Out of the Box'),
-        ),
-        body: GestureDetector(
-          child: Center(
-            child: Stack(
-              children: <Widget>[
-                buildBox(),
-                buildCatAnimation(), //cat will be at the bottom of the stack
-              ],
-              overflow: Overflow.visible,
-            ),
+      appBar: AppBar(
+        title: Text('Cat\'s Out of the Box'),
+      ),
+      body: GestureDetector(
+        child: Center(
+          child: Stack(
+            children: <Widget>[
+              buildCatAnimation(), //cat will be at the bottom of the stack
+              buildBox(),
+              buildLeftFlap(),
+              buildRightFlap(),
+            ],
+            overflow: Overflow.visible,
           ),
-          onTap: onTap,
-        ));
+        ),
+        onTap: onTap,
+      ),
+    );
   }
 
   Widget buildCatAnimation() {
@@ -82,6 +118,50 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
       width: 200.0,
       height: 200.0,
       color: Colors.brown,
+    );
+  }
+
+  Widget buildLeftFlap() {
+    return Positioned(
+      child: AnimatedBuilder(
+          animation: boxAnimation,
+          child: Container(
+            height: 12.0,
+            width: 130.0,
+            color: Colors.brown,
+          ),
+          builder: (context, child) {
+            return Transform.rotate(
+              child: child,
+              angle: boxAnimation.value,
+              alignment: Alignment
+                  .topLeft, //Move the rotation point of our flap to the top left.
+            );
+          }),
+      left: 3.0,
+      top: 1.0,
+    );
+  }
+
+  Widget buildRightFlap() {
+    return Positioned(
+      child: AnimatedBuilder(
+          animation: boxAnimation,
+          child: Container(
+            height: 12.0,
+            width: 130.0,
+            color: Colors.brown,
+          ),
+          builder: (context, child) {
+            return Transform.rotate(
+              child: child,
+              angle: -boxAnimation.value,
+              alignment: Alignment
+                  .topRight, //Move the rotation point of our flap to the top left.
+            );
+          }),
+      right: 3.0,
+      top: 1.0,
     );
   }
 }
