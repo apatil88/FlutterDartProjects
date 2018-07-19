831efee310da19a60b783ff920535617e49257de
Amrut Patil <apatil@emmes.com>
2018-07-19 16:40:28 -0400

    Initial commit and adding item model class

---
+++ flutter_newshack/lib/src/models/item_model.dart
@@ -0,0 +1,31 @@
+class ItemModel {
+  final int id;
+  final bool deleted;
+  final String type;
+  final String by;
+  final int time;
+  final String text;
+  final bool dead;
+  final int parent;
+  final List<dynamic> kids;
+  final String url;
+  final int score;
+  final String title;
+  final List<dynamic> descendants;
+
+  //Decode JSON and put in Model class
+  ItemModel.fromJson(Map<String, dynamic> parsedJson)
+      : id = parsedJson['id'],
+        deleted = parsedJson['deleted'],
+        type = parsedJson['type'],
+        by = parsedJson['by'],
+        time = parsedJson['time'],
+        text = parsedJson['text'],
+        dead = parsedJson['dead'],
+        parent = parsedJson['parent'],
+        kids = parsedJson['kids'],
+        url = parsedJson['url'],
+        score = parsedJson['score'],
+        title = parsedJson['title'],
+        descendants = parsedJson['descendants'];
+}
