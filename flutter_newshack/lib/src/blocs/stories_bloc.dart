ace9307226642f8b8ca2c1a3f00fe126dc4b99e0
Amrut Patil <apatil@emmes.com>
2018-07-23 15:48:14 -0400

    Adding PublishSubject as source of information and Observable as stream for widgets to consume information

--- flutter_newshack/lib/src/blocs/stories_bloc.dart
+++ flutter_newshack/lib/src/blocs/stories_bloc.dart
@@ -1,1 +1,21 @@
-class StoriesBloc {}
+import 'package:rxdart/rxdart.dart';
+import '../models/item_model.dart';
+import '../resources/repository.dart';
+
+class StoriesBloc {
+  final _repository = Repository();
+  final _topIds = PublishSubject<List<int>>(); //StreamController
+
+  //Getters to Streams
+  Observable<List<int>> get topIds => _topIds.stream;
+
+  //Get list of Ids from the Repository and add it to the sink on our topIds StreamController
+  fetchTopIds() async {
+    final ids = await _repository.fetchTopIds();
+    _topIds.sink.add(ids);
+  }
+
+  dispose() {
+    _topIds.close();
+  }
+}
