6b72cd07576d5264380490e61a60291e694534a8
Amrut Patil <apatil@emmes.com>
2018-07-23 15:00:52 -0400

    Adding BLoC for stories list, stories provider and dependency for rxdart

---
+++ flutter_newshack/lib/src/blocs/stories_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'stories_bloc.dart';
+
+class StoriesProvider extends InheritedWidget {
+  final StoriesBloc storiesBloc;
+
+  StoriesProvider({Key key, Widget child})
+      : storiesBloc = StoriesBloc(),
+        super(key: key, child: child);
+
+  bool updateShouldNotify(_) => true;
+
+  //Return the StoriesBloc instance to any widget in the widget hierarchy that wants access to the StoriesBloc instance.
+  //context here is the Widget context in the Widget hierarchy where we want access to the StoriesBloc instance
+  static StoriesBloc of(BuildContext context) {
+    return (context.inheritFromWidgetOfExactType(StoriesProvider)
+            as StoriesProvider)
+        .storiesBloc;
+  }
+}
--- flutter_newshack/lib/src/screens/news_list.dart
+++ flutter_newshack/lib/src/screens/news_list.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 class NewsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
         title: Text('Top HackNews'),
