3943f9b39af0eb427381e44f849a03c5b2923308
Amrut Patil <apatil@emmes.com>
2018-07-25 16:52:46 -0400

    Adding onGenerateRoute callback for navigation

--- flutter_newshack/lib/src/app.dart
+++ flutter_newshack/lib/src/app.dart
@@ -8,7 +8,13 @@ class App extends StatelessWidget {
     return StoriesProvider(
       child: MaterialApp(
         title: 'NewsHack',
-        home: NewsList(),
+        onGenerateRoute: (RouteSettings settings) {
+          return MaterialPageRoute(
+            builder: (context) {
+              return NewsList();
+            },
+          );
+        },
       ),
     );
   }
